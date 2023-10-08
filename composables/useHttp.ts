8387a74a10b310cb962de47f77316d8672b4d6aa
yong_deng <yong_deng@tungray.com.cn>
2023-10-08 13:28:58 +0800

    123

--- composables/useHttp.ts
+++ composables/useHttp.ts
@@ -14,7 +14,7 @@ export default async function useHttp(
   let result: any = null;
 
   // 发送请求
-  await useFetch(baseUrl + api, {
+  await $fetch(baseUrl + api, {
     method: requestMethod ?? "POST",
     body: requestBody,
     params: requestParams,
