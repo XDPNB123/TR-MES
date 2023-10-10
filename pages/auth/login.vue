d2c81e0304f5abcf9632ce69c41823a125ffd0ad
huaxiao168 <huaxiao168@126.com>
2023-10-10 15:26:35 +0800

    更改登陆页面标题

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -6,9 +6,9 @@ useSeoMeta({
   // 社交媒体分享该页面时显示的标题
   ogTitle: "登陆",
   // 该页面的描述
-  description: "同日 MES 系统，登陆",
+  description: "同日数字化工厂系统，登陆",
   // 社交媒体分享该页面时显示的描述
-  ogDescription: "同日 MES 系统，登陆",
+  ogDescription: "同日数字化工厂系统，登陆",
   // 社交媒体分享该页面时显示的图片
   ogImage: "/同日图标.png",
 });
