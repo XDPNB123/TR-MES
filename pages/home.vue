97d5649cecaae5c9743ae51d49417967a59e24dc
XDPNB123 <1914348429@qq.com>
2023-10-11 10:43:17 +0800

    1

---
+++ pages/home.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// 搜索引擎优化
+useSeoMeta({
+  // 该页面的标题
+  title: "主页",
+  // 社交媒体分享该页面时显示的标题
+  ogTitle: "主页",
+  // 该页面的描述
+  description: "同日 MES 系统，主页",
+  // 社交媒体分享该页面时显示的描述
+  ogDescription: "同日 MES 系统，主页",
+  // 社交媒体分享该页面时显示的图片
+  ogImage: "/同日图标.png",
+});
+</script>
+
+<template>
+  <div>
+    <client-only>
+      <Vue3Lottie animationLink="/主页.json" height="90vh" />
+    </client-only>
+  </div>
+</template>
