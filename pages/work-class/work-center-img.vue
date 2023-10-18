3cdd511cffe09a36d5d0c31f9534ec0d828c8dbe
XDPNB123 <1914348429@qq.com>
2023-10-18 14:37:10 +0800

    1

--- pages/work-class/work-center-img.vue
+++ pages/work-class/work-center-img.vue
@@ -1,2 +1,29 @@
-<script setup lang="ts"></script>
-<template>图形化界面</template>
+<script setup lang="ts">
+const arr = new Array(12).fill({
+  name: "工作中心",
+  status: "状态",
+  deprecated: "废弃",
+  free: "空闲",
+});
+</script>
+<template>
+  <v-row class="ma-3">
+    <v-col cols="3" v-for="(item, index) in arr" :key="index">
+      <v-card rounded="xl">
+        <v-img src="/头像.jpg" height="200px" cover class="align-center">
+          <v-card-title class="text-white">{{ item.name }}</v-card-title>
+          <v-divider></v-divider>
+          <v-card-subtitle class="text-white my-3">{{
+            item.status
+          }}</v-card-subtitle>
+          <v-card-subtitle class="text-white my-3">{{
+            item.deprecated
+          }}</v-card-subtitle>
+          <v-card-subtitle class="text-white my-3">{{
+            item.free
+          }}</v-card-subtitle>
+        </v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
