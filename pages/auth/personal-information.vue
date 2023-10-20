7940309c215a58a35bf709f1fe6381e3734bff8d
XDPNB123 <1914348429@qq.com>
2023-10-20 16:47:27 +0800

    1

--- pages/auth/personal-information.vue
+++ pages/auth/personal-information.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 // 搜索引擎优化
 useSeoMeta({
   // 该页面的标题
@@ -28,4 +28,50 @@ onMounted(() => {
 
 <template>
   <div class="initial-animation">这是个人信息页面</div>
+</template> -->
+
+<script setup lang="ts">
+import draggable from "vuedraggable";
+const list1 = ref([
+  { name: "John", id: 1 },
+  { name: "Joao", id: 2 },
+  { name: "Jean", id: 3 },
+  { name: "Gerard", id: 4 },
+]);
+const list2 = ref([
+  { name: "Juan", id: 5 },
+  { name: "Edgard", id: 6 },
+  { name: "Johnson", id: 7 },
+]);
+</script>
+
+<template>
+  <v-row class="mt-3">
+    <v-col cols="2">
+      <v-card width="200">
+        <draggable :list="list1" group="people" itemKey="name">
+          <template #item="{ element, index }">
+            <v-list-item
+              :border="true"
+              :title="element.name + '>' + index"
+            ></v-list-item>
+          </template>
+        </draggable>
+      </v-card>
+    </v-col>
+    <v-col cols="2">
+      <v-card width="200">
+        <draggable :list="list2" group="people" itemKey="name">
+          <template #item="{ element, index }">
+            <v-list-item
+              :border="true"
+              :title="element.name + '>' + index"
+            ></v-list-item>
+          </template>
+        </draggable>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+<style scoped></style>
