b3c6a68fb0d389c96cbaddd9689ef6d50dc40d6d
XDPNB123 <1914348429@qq.com>
2023-10-20 11:49:47 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,11 +32,12 @@ const userMenus = ref<any[]>([
 // 此处定义页面菜单
 const pageMenus = ref<any[]>([
   {
-    title: "看板",
+    name: "看板",
     path: "/dashboard",
     icon: "fa-solid fa-chart-column",
     children: [
       {
+        parentName: "看板",
         name: "主看板",
         path: "/dashboard/main-dashboard",
         icon: "fa-solid fa-boxes-stacked",
@@ -44,26 +45,30 @@ const pageMenus = ref<any[]>([
     ],
   },
   {
-    title: "MES",
+    name: "MES",
     path: "/mes",
     icon: "fa-solid fa-table-list",
     children: [
       {
+        parentName: "MES",
         name: "生产工单",
         path: "/mes/work-order-product",
         icon: "fa-solid fa-table-list",
       },
       {
+        parentName: "MES",
         name: "工单排产",
         path: "/mes/work-produce-recode",
         icon: "fa-solid fa-list-ol",
       },
       {
+        parentName: "MES",
         name: "常用工序",
         path: "/mes/process-maintenance",
         icon: "fa-solid fa-screwdriver-wrench",
       },
       {
+        parentName: "MES",
         name: "BOM清单",
         path: "/mes/bom-list",
         icon: "fa-solid fa-list",
@@ -71,11 +76,12 @@ const pageMenus = ref<any[]>([
     ],
   },
   {
-    title: "WMS",
+    name: "WMS",
     path: "/wms",
     icon: "fa-solid fa-chart-column",
     children: [
       {
+        parentName: "WMS",
         name: "仓库主页",
         path: "/wms/home",
         icon: "fa-solid fa-gauge",
@@ -83,21 +89,24 @@ const pageMenus = ref<any[]>([
     ],
   },
   {
-    title: "基础信息",
+    name: "基础信息",
     path: "/work-class",
     icon: "fa-solid fa-users-rays",
     children: [
       {
+        parentName: "基础信息",
         name: "工作中心",
         path: "/work-class/work-center",
         icon: "fa-solid fa-location-crosshairs",
       },
       {
+        parentName: "基础信息",
         name: "工作中心图形化界面",
         path: "/work-class/work-center-img",
         icon: "fa-solid fa-location-crosshairs",
       },
       {
+        parentName: "基础信息",
         name: "班组信息",
         path: "/work-class/work-class",
         icon: "fa-solid fa-people-roof",
@@ -107,46 +116,47 @@ const pageMenus = ref<any[]>([
 ]);
 
 // tabs 类型
-type tabType = {
+type TabType = {
+  parentName: string;
   name: string;
   path: string;
   icon: string;
 };
 
 // 选中的 tab
-let selectTab = ref<string>("");
+let selectTab = ref<TabType | null>(null);
 
 // 正在显示的所有 tab
-let tabs = ref<tabType[]>([]);
+let tabs = ref<TabType[]>([]);
 
 // 添加 tab
-function addTab(tab: tabType) {
+function addTab(tab: TabType) {
   // 新的 tab 是否在 tabs 中已经存在，存在则不添加
   if (tabs.value.findIndex((item) => item.name === tab.name) < 0)
     tabs.value.push(tab);
   // 更新选中的 tab
-  selectTab.value = tab.name;
+  selectTab.value = tab;
 }
 
 // 移除 tab
-function removeTab(tab: tabType) {
+function removeTab(tab: TabType) {
   // 获取要删除的 tab 的索引
   const tabIndex = tabs.value.indexOf(tab);
   // 从 tabs 中删除该 tab
   tabs.value.splice(tabIndex, 1);
 
   // 如果删除的 tab 为选中的 tab
-  if (tab.name === selectTab.value) {
+  if (selectTab.value && tab.name === selectTab.value.name) {
     // 如果删除该 tab 后，tabs 长度大于 0
     if (tabs.value.length > 0) {
       // 如果删除的 tab 为最后一个，则需要更新选中的 tab 为前一个 tab，且跳转到前一个 tab
       if (tabIndex === tabs.value.length) {
-        selectTab.value = tabs.value[tabIndex - 1].name;
+        selectTab.value = tabs.value[tabIndex - 1];
         router.push({ path: tabs.value[tabIndex - 1].path });
         return;
       }
       // 如果删除的 tab 不为最后一个，则需要更新选中的 tab 为后一个 tab，且跳转到后一个 tab
-      selectTab.value = tabs.value[tabIndex].name;
+      selectTab.value = tabs.value[tabIndex];
       router.push({ path: tabs.value[tabIndex].path });
     }
     // 如果删除该 tab 后，tabs 长度等于 0，则不需要做任何操作
@@ -174,7 +184,10 @@ function exit() {
 watch(
   tabs,
   () => {
-    if (tabs.value.length === 0) router.push({ path: "/home" });
+    if (tabs.value.length === 0) {
+      selectTab.value = null;
+      router.push({ path: "/home" });
+    }
   },
   { deep: true }
 );
@@ -240,62 +253,14 @@ watch(
 
       <template v-slot:extension>
         <div
-          class="w-100 d-flex justify-space-between"
+          class="w-100 d-flex justify-space-between pl-3 mt-1"
           style="background-color: white"
         >
-          <v-menu
-            open-on-hover
-            open-delay="0"
-            close-delay="100"
-            :close-on-content-click="false"
-          >
-            <template v-slot:activator="{ props }">
-              <div>
-                <v-btn variant="flat" v-bind="props" class="rounded-0">
-                  <v-badge :content="tabs.length" color="green">
-                    <v-icon>fa-regular fa-folder-open</v-icon>
-                  </v-badge>
-                </v-btn>
-                <span
-                  v-show="selectTab"
-                  class="font-weight-bold text-h6 menu-active mt-1"
-                >
-                  {{ selectTab }}页面
-                </span>
-              </div>
-            </template>
-
-            <v-list rounded="lg" class="pa-3">
-              <!-- 当 url 匹配到 to 时，就会触发 active-class -->
-              <v-list-item
-                class="mb-1"
-                rounded="lg"
-                active-class="list-item-active"
-                v-for="(item, index) in tabs"
-                :key="index"
-                :to="item.path"
-                @click="selectTab = item.name"
-              >
-                <template v-slot:prepend>
-                  <v-icon class="mr-6">{{ item.icon }}</v-icon>
-                  <div>{{ item.name }}</div>
-                </template>
-                <template v-slot:append>
-                  <v-icon @click.prevent="removeTab(item)"
-                    >fa-solid fa-xmark</v-icon
-                  >
-                </template>
-              </v-list-item>
-
-              <v-list-item class="mb-1" rounded="lg" @click="tabs = []">
-                <template v-slot:prepend>
-                  <v-icon class="mr-6">fa-solid fa-trash</v-icon>
-                  <div v-if="tabs.length > 0">关闭所有</div>
-                  <div v-else>没有打开任何页面</div>
-                </template>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+          <div class="text-h6 font-weight-medium text-blue-darken-2">
+            {{ selectTab?.parentName ?? "" }}
+            <span v-show="selectTab">/</span>
+            {{ selectTab?.name ?? "" }}
+          </div>
 
           <div>
             <v-menu
@@ -309,14 +274,14 @@ watch(
                 <v-btn
                   class="rounded-0"
                   :class="{
-                    'menu-active':
+                    'text-blue-darken-2':
                       router.currentRoute.value.fullPath.startsWith(item.path),
                   }"
                   variant="flat"
                   v-bind="props"
                 >
                   <v-icon>{{ item.icon }}</v-icon>
-                  <div class="mx-6">{{ item.title }}</div>
+                  <div class="mx-6">{{ item.name }}</div>
                   <v-icon>fa-solid fa-angle-down</v-icon>
                 </v-btn>
               </template>
@@ -336,6 +301,53 @@ watch(
                 </v-list-item>
               </v-list>
             </v-menu>
+
+            <v-menu
+              open-on-hover
+              open-delay="0"
+              close-delay="100"
+              :close-on-content-click="false"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn variant="flat" v-bind="props" class="rounded-0">
+                  <v-badge color="green" class="mr-3" :content="tabs.length">
+                    <v-icon>fa-regular fa-folder-open</v-icon>
+                  </v-badge>
+                  已打开的页面
+                </v-btn>
+              </template>
+
+              <v-list rounded="lg" class="pa-3">
+                <!-- 当 url 匹配到 to 时，就会触发 active-class -->
+                <v-list-item
+                  class="mb-1"
+                  rounded="lg"
+                  active-class="list-item-active"
+                  v-for="(item, index) in tabs"
+                  :key="index"
+                  :to="item.path"
+                  @click="selectTab = item"
+                >
+                  <template v-slot:prepend>
+                    <v-icon class="mr-6">{{ item.icon }}</v-icon>
+                    <div>{{ item.name }}</div>
+                  </template>
+                  <template v-slot:append>
+                    <v-icon @click.prevent="removeTab(item)"
+                      >fa-solid fa-xmark</v-icon
+                    >
+                  </template>
+                </v-list-item>
+
+                <v-list-item class="mb-1" rounded="lg" @click="tabs = []">
+                  <template v-slot:prepend>
+                    <v-icon class="mr-6">fa-solid fa-trash</v-icon>
+                    <div v-if="tabs.length > 0">关闭所有</div>
+                    <div v-else>没有打开任何页面</div>
+                  </template>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </div>
         </div>
       </template>
@@ -351,9 +363,5 @@ watch(
 .list-item-active {
   background-image: linear-gradient(25deg, #0044bd, #1e66d3, #2a88e9, #2dacff);
   color: white !important;
-}
-.menu-active {
-  color: #1976d2;
-  border-bottom: 7px solid #1976d2;
 }
 </style>
