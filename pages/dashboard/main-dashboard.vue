ab5fc222e69c49fb4ebdd16597ca3cd8e4c36813
yong_deng <yong_deng@tungray.com.cn>
2023-12-29 10:58:30 +0800

    1

--- pages/dashboard/main-dashboard.vue
+++ pages/dashboard/main-dashboard.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <iframe
-    src="http://10.0.20.61:8051/work-order"
-    width="100%"
-    height="100%"
-  ></iframe>
+  <iframe src="http://10.0.20.61:8051" width="100%" height="100%"></iframe>
 </template>
